1b8cd9513c03336f0f7b955a73f5be1776cd5d96
Kukhyeon Heo <sainthkh@gmail.com>
2023-11-17 18:41:37 +0900

    Add Scene.cs & Reorganize files.

---
+++ src/Scene.cs
@@ -0,0 +1,7 @@
+using Microsoft.Xna.Framework;
+
+public class Scene {
+    public virtual void Load() {}
+    public virtual void Update(GameTime gameTime) {}
+    public virtual void Draw(GameTime gameTime) {}
+}
